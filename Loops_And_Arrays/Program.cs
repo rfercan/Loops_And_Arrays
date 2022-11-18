cb306043900a4532bfecefecd624095d27b587dd
Fatih <r.fatihercan@hotmail.com>
2022-11-18 23:58:51 +0300

    Add project files.

---
+++ Loops_And_Arrays/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+string course1 = "C";
+string course2 = "C++";
+string course3 = "C#";
+string[] courses = new string[] { course1, course2, course3 };
+
+Console.WriteLine("------Using For-----------");
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i]);
+}
+Console.WriteLine("------Using Foreach-----------");
+foreach (var course in courses)
+{
+    Console.WriteLine(course);
+}
